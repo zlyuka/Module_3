a1f216071210026218ebc5234152186e0f309350
zlyuka <zhenya0699@gmail.com>
2024-09-18 16:42:58 +0300

    Установлен счетчик, добавлена функция string_info частично функция is_comtains

--- Module_3.1.py
+++ Module_3.1.py
@@ -1,13 +1,30 @@
-
+calls = 0
+list_to_search = []
 
 def count_calls():
+    global calls
+    calls += 1
+    return calls
 
 
-    pass
 
-def string_info():
-    pass
+def string_info(string):
+    len(string)
+    string.lower()
+    string.upper()
+    count_calls()
+    print(f"{len(string)},'{string.upper()}','{string.lower()}'")
 
 
-def is_comtains():
-    pass
+# def is_comtains(string, list_to_search):
+#     for str in list_to_search:
+#         print(str == list_to_search):
+#     pass
+
+list_to_search.append(input('Введите список в котором будет искаться соответствие '))
+
+print(list_to_search)
+string = input('Введите строку')
+string_info(string)
+# is_comtains()
+print(calls)
