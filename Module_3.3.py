4da225283a9e3ab37c20983881845004fb9d82ed
zlyuka <zhenya0699@gmail.com>
2024-09-20 15:30:39 +0300

    Задание выполнено.

---
+++ Module_3.3.py
@@ -0,0 +1,13 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+# values_list = [1, 'Ня', True]
+# values_dict = {'a': 2, 'b': 'nya', 'c': False}
+# print_params(*values_list)
+# print_params(**values_dict)
+#
+# values_list2 = [1, 'nyaaaa']
+# print_params(*values_list2, 42)
+
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list_2, 42)
