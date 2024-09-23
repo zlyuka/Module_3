5d5b8b5f05456bf24186ceadc4a399bd04cad1c2
Zlyuka <zhenya0699@gmail.com>
2024-09-23 22:12:05 +0300

    Доработанно.

--- Module_3.5.py
+++ Module_3.5.py
@@ -2,10 +2,16 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
-    if len(str_number) > 1:
+    if first == 0:
+        first = 1
+    elif len(str_number) > 1:
         first = first * get_multiplied_digits(int(str_number[1:]))
     return first
 
-
 result = get_multiplied_digits(40203)
 print(result)
+result = get_multiplied_digits(420)
+print(result)
+
+
+
