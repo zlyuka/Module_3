7a78ece7a76174733c8345b13a6db322c3223f05
Zlyuka <zhenya0699@gmail.com>
2024-09-29 15:01:31 +0300

    Доработанно.

--- Module_3.5.py
+++ Module_3.5.py
@@ -1,17 +1,44 @@
+#
+# def get_multiplied_digits(number):
+#     str_number = str(number)
+#     first = int(str_number[0])
+#     if first == 0:
+#         first = 1
+#     elif len(str_number) > 1:
+#         first = first * get_multiplied_digits(int(str_number[1:]))
+#     return first
+#
+# result = get_multiplied_digits(40203)
+# print(result)
+# result = get_multiplied_digits(420)
+# print(result)
+
 
 def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
-    if first == 0:
-        first = 1
-    elif len(str_number) > 1:
-        first = first * get_multiplied_digits(int(str_number[1:]))
-    return first
+    if len(str_number) > 1:
+        return  first * get_multiplied_digits(int(str_number[1:]))
+    if first != 0:
+        return first
+    return 1
+
+
 
 result = get_multiplied_digits(40203)
 print(result)
 result = get_multiplied_digits(420)
 print(result)
 
-
-
+# def get_multiplied_digits(number):
+#     str_number = str(number)
+#     first = int(str_number[0])
+#     for int in number:
+#     if first == 0:
+#         first = 1
+#     if len(str_number) > 1:
+#         first = first * get_multiplied_digits(int(str_number[1:]))
+#     return first
+#
+# result = get_multiplied_digits(420)
+# print(result)
